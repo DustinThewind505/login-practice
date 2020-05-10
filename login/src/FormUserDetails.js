813bb4de2591e573bd7ad81a4cdcb51677200691
unknown <Dustin.Guillen14@yahoo.com>
2020-05-09 22:01:36 -0600

    began adding server

--- login/src/FormUserDetails.js
+++ login/src/FormUserDetails.js
@@ -6,26 +6,59 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 export class FormUserDetails extends Component {
-continue = event => {
-    event.preventDefault();
+  continue = e => {
+    e.preventDefault();
     this.props.nextStep();
-}
-    
-    render() {
-        const { values, handleChange } = this.props;
-        return (
-            <MuiThemeProvider>
-                <React.Fragment>
-                <AppBar/>
-                <TextField
-                hintText='enter Your first name'
-                floatingLabelText='First Name'
-                onChange={handleChange('firstName')}
-                    />
-                </React.Fragment>
-            </MuiThemeProvider>
-        );
-    }
+  };
+
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider >
+        <React.Fragment>
+          <Dialog 
+            open="true"
+            fullWidth="true"
+            maxWidth='sm'
+          >
+            <AppBar title="Enter User Details" />
+            <TextField
+              placeholder="Enter Your First Name"
+              label="First Name"
+              onChange={handleChange('firstName')}
+              defaultValue={values.firstName}
+              margin="normal"
+							fullWidth="true"
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Last Name"
+              label="Last Name"
+              onChange={handleChange('lastName')}
+              defaultValue={values.lastName}
+              margin="normal"
+							fullWidth="true"
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Email"
+              label="Email"
+              onChange={handleChange('email')}
+              defaultValue={values.email}
+              margin="normal"
+							fullWidth="true"
+            />
+            <br />
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Continue</Button>
+          </Dialog>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default FormUserDetails;
