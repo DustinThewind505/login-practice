7e1f252a97cb89fd5ee090bda18bfdeafa303336
unknown <Dustin.Guillen14@yahoo.com>
2020-05-10 23:02:45 -0600

    addedserver.get and server.post requests

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
 
 const PORT = 420;
+
+server.use(express.json());
+
+let hikers = [
+    {
+        "id": 0,
+        "name": "Zig",
+        "bio": "She's hardcore"
+    },
+    {
+        "id": 1,
+        "name": "Zig",
+        "bio": "She's hardcore"
+    }
+]
+
+server.get('/api/hikers', (req, res) => {
+    res.status(200).json(hikers)
+})
+
+server.post('/api/hikers', (req, res) => {
+    const newHiker = req.body;
+
+    newHiker.id = shortid.generate();
+
+    hikers.push(newHiker);
+
+    res.status(201).json(newHiker);
+})
 
 server.listen(PORT, () => 
     console.log(`\n ** API listening on http://localhost${PORT} ** \n`)
