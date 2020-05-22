4d03bc5292e1b98c297d874daf9205ac7e1342e0
unknown <Dustin.Guillen14@yahoo.com>
2020-05-21 22:19:10 -0600

    added PUT and DELETE

--- posts/post-router.js
+++ posts/post-router.js
@@ -18,8 +18,13 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     db('posts').where({ id: req.params.id })
-    .then(rows => {
-        res.status(200).json({data: rows})
+    .first()
+    .then(post => {
+        if(post){
+            res.status(200).json({data: post})
+        } else {
+            res.staatus(404).json({message: 'post not found'})
+        }
     })
     .catch(error => {
         res.status(500).json({message: error})
@@ -37,11 +42,35 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+    db('posts').where({id: req.params.id})
+    .update(req.body)
+    .then(count => {
+        if(count) {
+            res.status(200).json({message: 'post was updated'})
+        } else {
+            res.status(404).json({message: 'post not found'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({message: 'Esorry', le_error: error})
+    })
 
 });
 
 router.delete('/:id', (req, res) => {
-
+    db('posts').where({id: req.params.id})
+    .delete()
+    .then(count => {
+        if(count){
+            console.log(count)
+            res.status(200).json({message: `deleted post`})
+        } else {
+            res.status(404).json({message: `post not found`})
+        }
+    })
+    .catch(error => {
+        res.status(500)/json({message: 'Esorry', le_error: error})
+    })
 });
 
 module.exports = router;
