95b3c152199ae6598dcec095c5afeb13137dabc7
unknown <Dustin.Guillen14@yahoo.com>
2020-05-21 21:26:06 -0600

    used SQL like javascript on crud operations

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,15 +6,34 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    db.select('*').from('posts')
+        .then(rows => {
+            res.status(200).json({data: rows})
+        })
+        .catch(error => {
+            res.status(500).json({message: 'Esorry', le_error: error})
+        })
+    
 });
 
 router.get('/:id', (req, res) => {
-
+    db('posts').where({ id: req.params.id })
+    .then(rows => {
+        res.status(200).json({data: rows})
+    })
+    .catch(error => {
+        res.status(500).json({message: error})
+    })
 });
 
 router.post('/', (req, res) => {
-
+    db('posts').insert(req.body, 'id')
+    .then(ids => {
+        res.status(201).json({ results: ids})
+    })
+    .catch(error => {
+        res.status(500)/json({message: 'Esorry', le_error: error})
+    })
 });
 
 router.put('/:id', (req, res) => {
