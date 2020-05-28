c30bd926045ed03d466a25d24fff1999582fc754
unknown <Dustin.Guillen14@yahoo.com>
2020-05-27 20:17:06 -0600

    began guided project db3

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,22 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, 
     connection: {
-      filename: './dev.db3'
-    }, 
-    useNullAsDefault: true
+      filename: './data/blog.db3',
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
-
-
 };
