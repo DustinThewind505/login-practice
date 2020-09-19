68da3eec7ed4ccfbcede7b26ca729e4cbd4ef591
unknown <Dustin.Guillen14@yahoo.com>
2020-09-18 21:43:38 -0600

    avengers component character list

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-// import logo from './logo.svg';
-
-// import { Card, Container, Row, Col } from 'reactstrap';
-
-// import Counter from './components/counter/counter';
-// import ColorPicker from './components/colorPicker/colorPicker';
-// import HideElement from './components/hideElement/hideElement';
-// import InputValue from './components/inputValue/inputValue';
-// import UserProps from './components/userProps/userProps';
-// import RandomColor from './components/randomColors/randomColor';
-// import GhibliApi from './components/ghibliApi/ghibliAPI';
-// import DogApi from './components/dogAPI/dogAPI';
-// import RickAndMortyApi from './components/rickAndMorty/RickAndMorty';
-// import NasaApi from './components/nasaApi/nasaApi';
 
 import PageOne from './components/PAGE1/page1';
+import AvengersApp from './components/PAGE2/avengersApp';
 
 import './App.css';
+
+const Home = props => <h1>Home Component</h1>;
 
 
 function App() {
 
 
-
-
   return (
     <div className="App">
       <header className="App-header">
-        <Route path='/' component={PageOne} />
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/page1'>Page One</Link></li>
+          <li><Link to='/page2' >Avengers APP</Link></li>
+        </ul>
+        <Route exact path='/' component={Home} />
+        <Route path='/page1' component={PageOne} />
+        <Route path='/page2' component={AvengersApp} />
       </header>
     </div>
   );
