d4e77170995d9312726b471be968d6223b2897a1
unknown <Dustin.Guillen14@yahoo.com>
2020-09-23 22:00:18 -0600

    add useRouteMatch hook/ deconstructed { path,url }

--- react-practice/src/components/PAGE2/avengerBio.js
+++ react-practice/src/components/PAGE2/avengerBio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, useRouteMatch } from 'react-router-dom';
 
 import AvengerInfo from './avengerDescription';
 import AvengerMovies from './avengerMovies';
@@ -9,6 +9,8 @@ function AvengerBio(props) {
 
   const avenger = props.avengersData.find(hero => hero.id === Number(props.match.params.id))
 
+  const { path, url } = useRouteMatch();
+
   return (
     <>
       <img className='character-image' src={avenger.img} alt={avenger.name} />
@@ -16,13 +18,13 @@ function AvengerBio(props) {
       <h1>{avenger.name}</h1>
       <h3>({avenger.nickname})</h3>
       <ul className='bio-list'>
-        <li><NavLink to={`/avengers/${avenger.id}/info`}>Bio</NavLink></li>
-        <li><NavLink to={`/avengers/${avenger.id}/movies`}>Movies</NavLink></li>
+        <li><NavLink to={`${url}/info`}>Bio</NavLink></li>
+        <li><NavLink to={`${url}/movies`}>Movies</NavLink></li>
       </ul>
       
       
-      <Route path='/avengers/:id/info' render={() => <AvengerInfo {...props} avenger={avenger} />} />
-      <Route exact path='/avengers/:id/movies' render={() => <AvengerMovies {...props} avenger={avenger} />} />
+      <Route path={`${path}/info`} render={props => <AvengerInfo {...props} avenger={avenger} />} />
+      <Route path={`${path}/movies`} render={props => <AvengerMovies {...props} avenger={avenger} />} />
       </div>
       <button onClick={() => props.history.push('/avengers')}>Go Back</button>
     </>
--- react-practice/src/components/PAGE2/avengersApp.js
+++ react-practice/src/components/PAGE2/avengersApp.js
@@ -10,7 +10,7 @@ function AvengersApp(props){
     return(
         <div className='characters-list-wrapper'>
           {props.avengersData.map(avenger => 
-            <div onClick={`/avengers/${avenger.id}`} className='character-card' key={avenger.id} onClick={() => props.history.push(`avengers/${avenger.id}`)}>
+            <div className='character-card' key={avenger.id} onClick={() => props.history.push(`avengers/${avenger.id}`)}>
               <img src={avenger.thumbnail} alt={avenger.name} />
               <h2>{avenger.name}</h2>
               <h3>({avenger.nickname})</h3>
