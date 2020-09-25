21efac42788d8bc3ab2a1a6f336fb03ae9ec471d
unknown <Dustin.Guillen14@yahoo.com>
2020-09-24 20:45:11 -0600

    used onChange handler in inputValue.js component

--- react-practice/src/components/inputValue/inputValue.js
+++ react-practice/src/components/inputValue/inputValue.js
@@ -4,8 +4,10 @@ import { CardHeader, CardBody, CardText } from 'reactstrap';
 
 
 function InputValue(props) {
-    const [title, setTitle] = useState("Title")
-    const [body, setBody] = useState("Body")
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [dropdown, setDropdown] = useState("");
+    const [happyCheckbox, setHappyCheckbox] = useState(false);
 
 
 
@@ -13,12 +15,35 @@ function InputValue(props) {
         <>
             <CardHeader><span style={props.fontStyles}>Input</span> V<span style={props.fontStyles}>a</span>l<span style={props.fontStyles}>u</span><span style={props.fontStyles}>e</span></CardHeader>
             <CardBody>
-            <div className="input-value-note" style={{backgroundColor: props.randomColor}}>
-                <CardText><strong>{title}</strong></CardText>
-                <CardText>{body}</CardText>
-            </div>
-            <input onChange={event => setTitle(event.target.value)} placeholder="Enter Title" />
-            <textarea onChange={event => setBody(event.target.value)} placeholder="Enter Body" />
+                <div className="input-value-note" style={{ backgroundColor: props.randomColor }}>
+                    <CardText><strong>{title || "Title"}</strong></CardText>
+                    <CardText>{body || "Body"}</CardText>
+                    <CardText style={props.fontStyles}>{dropdown || "Dropdown"}</CardText>
+                    <CardText>Are you happy? {happyCheckbox ? "Yes" : "No"}</CardText>
+                </div>
+                <label>
+                    <input id="inputTitle" onChange={event => setTitle(event.target.value)} placeholder="Enter Title" />
+                </label>
+                <br />
+                <br />
+                <label>
+                    <textarea id="textareaBody" onChange={event => setBody(event.target.value)} placeholder="Enter Body" />
+                </label>
+                <br />
+                <br />
+                <label>
+                    <select id="dropdownMonth" onChange={event => setDropdown(event.target.value)} style={props.fontStyles} >
+                        <option></option>
+                        <option>April</option>
+                        <option>May</option>
+                        <option>June</option>
+                    </select>
+                </label>
+                <br />
+                <br />
+                <label>
+                    Happy: <input id="checkboxHappy" type="checkbox" onChange={event => setHappyCheckbox(event.target.checked)} />
+                </label>
             </CardBody>
         </>
     )
