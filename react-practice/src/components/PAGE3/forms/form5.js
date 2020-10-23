1a62bc9151220c280a29e1f685353353901c038d
unknown <Dustin.Guillen14@yahoo.com>
2020-10-23 12:34:24 -0600

    form5  inline & full form validation w/ yup

--- react-practice/src/components/PAGE3/forms/form5.js
+++ react-practice/src/components/PAGE3/forms/form5.js
@@ -1,36 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+// ========== From validation with yup ==========
+const formSchema = yup.object().shape({
+    number: yup.number().positive("Must be positive")
+})
 
 
-
-function Form5() {
+function Form5(props) {
     // ========== STATE ==========
+    const [disableButton, setDisableButton] = useState(true)
 
+    const [formData, setFormData] = useState({
+        number: 0
+    })
 
-
+    const [errorsState, setErrorsState] = useState({
+        number: ""
+    })
 
 
 
     // ========== FUNCTIONS ==========
+    const validateInput = (e) => {
+        yup.reach(formSchema, "number").validate(e.target.value)
+            .then(res => setErrorsState({ number: "" }))
+            .catch(err => setErrorsState({number: err.errors[0]}))
+    }
+
+    const handleChange = (e) => {
+        e.persist()
+        const newFormState = {
+            number: e.target.value
+        }
+
+        validateInput(e)
+        setFormData(newFormState)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        props.setDisplayForm(formData)
+
+        setFormData({
+            number: 0
+        })
+    }
+
+    useEffect(() => {
+        formSchema.isValid(formData).then(res => setDisableButton(!res))
+    }, [formData])
 
 
 
     // ========== COMPONENT ==========
-    return(
-       <div className='form-container'>
-           <h3>Form #5</h3>
-           <p>Number:</p>
-           <form>
-               <h3>Form #5 - input/number</h3>
-               <section className='form-body'>
-                   <label>number
-                       <input type='number' />
-                   </label>
-               </section>
-               <footer>
-                   <button type='submit'>Submit</button>
-               </footer>
-           </form>
-       </div>
+    return (
+        <div className='form-container'>
+            <h3>Form #5</h3>
+            <p>Number: {formData.number}</p>
+            <form onSubmit={handleSubmit}>
+                <h3>Form #5 - input/number</h3>
+                <section className='form-body'>
+                    <label>number
+                       <input type='number' value={formData.number} onChange={handleChange} />
+                        {errorsState.number.length > 0 ? <p className='error'>{errorsState.number}</p> : null}
+                    </label>
+                </section>
+                <footer>
+                    <button type='submit' disabled={disableButton}>Submit</button>
+                </footer>
+            </form>
+        </div>
     )
 }
 
