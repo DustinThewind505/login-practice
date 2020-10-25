e71086c637f98eada56e43fb46feab06f1802f70
unknown <Dustin.Guillen14@yahoo.com>
2020-10-25 12:33:58 -0600

    radio input group

--- react-practice/src/components/PAGE3/forms/form6.js
+++ react-practice/src/components/PAGE3/forms/form6.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 function Form6(props) {
+    // ========== STATE ==========
+    const [formData, setFormData] = useState({
+        radio: ""
+    })
 
 
 
 
+
+    // ========== FUNCTIONS ==========
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
+    const handleChange = e => {
+        // const newFormState = {
+        //     radio: e.target.value
+        // }
+
+        // setFormData(newFormState)
+        console.log(e.target.value)
+    }
+
+
+
+    // ========== COMPONENT ==========
     return(
         <div className='form-container'>
             <h3>Form #6</h3>
             <p>Radio:</p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h3>Form #6</h3>
                 <section className='form-body'>
-                    <label>
-                        <input type='radio' />
+                    <label>Coke
+                        <input type='radio' name='soda' value='coke' onChange={handleChange}/>
+                    </label>
+                    <label>Sprite
+                        <input type='radio' name='soda' value='sprite' onChange={handleChange}/>
+                    </label>
+                    <label>Cream Soda
+                        <input type='radio' name='soda' value='cream soda🍺' onChange={handleChange}/>
+                    </label>
+                    <label>Sierra Mist
+                        <input type='radio' name='soda' value='sierra mist' onChange={handleChange}/>
                     </label>
                 </section>
                 <footer>
