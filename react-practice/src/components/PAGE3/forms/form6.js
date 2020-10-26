ddb479a734349c6d2c872a1904af2f0962c9c955
unknown <Dustin.Guillen14@yahoo.com>
2020-10-26 13:38:24 -0600

    form6 form validation functionality

--- react-practice/src/components/PAGE3/forms/form6.js
+++ react-practice/src/components/PAGE3/forms/form6.js
@@ -1,56 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+// ========== Form validation with yup ==========
+const formSchema = yup.object().shape({
+    soda: yup.string().oneOf(["coke", "cream soda🍺", "rc cola"], "No Pepsi!")
+})
+
 
 
 
 function Form6(props) {
     // ========== STATE ==========
+    const [disableButton, setDisableButton] = useState(true)
+
     const [formData, setFormData] = useState({
-        radio: ""
+        soda: ""
+    })
+
+    const [errorsState, setErrorsState] = useState({
+        soda: ""
     })
 
 
 
 
-
     // ========== FUNCTIONS ==========
+    const validateInput = e => {
+        yup.reach(formSchema, "soda").validate(e.target.value)
+            .then(res => setErrorsState({ soda: "" }))
+            .catch(err => setErrorsState({ soda: err.errors[0] }))
+    }
+
     const handleSubmit = e => {
         e.preventDefault()
+
+        props.setDisplayForm(formData)
+
+        setFormData({
+            soda: ""
+        })
     }
 
     const handleChange = e => {
-        // const newFormState = {
-        //     radio: e.target.value
-        // }
+        e.persist()
+        const newFormState = {
+            soda: e.target.value
+        }
 
-        // setFormData(newFormState)
-        console.log(e.target.value)
+        validateInput(e)
+        setFormData(newFormState)
     }
+
+    useEffect(() => {
+        formSchema.isValid(formData).then(res => setDisableButton(!res))
+    })
 
 
 
     // ========== COMPONENT ==========
-    return(
+    return (
         <div className='form-container'>
             <h3>Form #6</h3>
-            <p>Radio:</p>
+            <p>Soda: {formData.soda}</p>
             <form onSubmit={handleSubmit}>
                 <h3>Form #6</h3>
                 <section className='form-body'>
-                    <label>Coke
-                        <input type='radio' name='soda' value='coke' onChange={handleChange}/>
-                    </label>
-                    <label>Sprite
-                        <input type='radio' name='soda' value='sprite' onChange={handleChange}/>
-                    </label>
-                    <label>Cream Soda
-                        <input type='radio' name='soda' value='cream soda🍺' onChange={handleChange}/>
-                    </label>
-                    <label>Sierra Mist
-                        <input type='radio' name='soda' value='sierra mist' onChange={handleChange}/>
-                    </label>
+                    <div className='radio-container'>
+                        <label>Coke
+                        <input type='radio' name='soda' value='coke' onChange={handleChange} />
+                        </label>
+                        <label>RC Cola
+                        <input type='radio' name='soda' value='rc cola' onChange={handleChange} />
+                        </label>
+                        <label>Cream Soda
+                        <input type='radio' name='soda' value='cream soda🍺' onChange={handleChange} />
+                        </label>
+                        <label>Pepsi
+                        <input type='radio' name='soda' value='pepsi' onChange={handleChange} />
+                            
+                        </label>
+                    </div>
+                    {errorsState.soda.length > 0 ? <p className='error'>{errorsState.soda}</p> : null}
                 </section>
                 <footer>
-                    <button type='submit'>Submit</button>
+                    <button type='submit' disabled={disableButton}>Submit</button>
                 </footer>
             </form>
         </div>
