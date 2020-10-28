036b43739030a951284096c907b429efd616538a
unknown <Dustin.Guillen14@yahoo.com>
2020-10-28 16:54:38 -0600

    disable button with if statement

--- react-practice/src/components/PAGE3/default/defaultContainer.js
+++ react-practice/src/components/PAGE3/default/defaultContainer.js
@@ -1,20 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 
 
 function DefaultFormBehavior(props) {
+    // ========== STATE ==========
+    const [disableButton, setDisableButton] = useState(true)
+
+    const [formData, setFormData] = useState({
+        username: ""
+    })
+
+
+    // ========== FUNCTIONS ==========
+    const handleChange = e => {
+        const newFormState = {
+            username: e.target.value
+        }
+
+        setFormData(newFormState)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        props.setDisplayForm(formData)
+
+        setFormData({
+            username: ""
+        })
+    }
+
+    useEffect(() => {
+        if(formData.username === ""){
+            setDisableButton(true)
+        } else {
+            setDisableButton(false)
+        }
+    }, [formData])
 
 
 
+    // ========== COMPONENTS ==========
     return (
         <div className='forms-page-container'>
-            <form>
-                <label>
-                    <input />
-                </label>
-
+            <form onSubmit={handleSubmit}>
+                <h3>Username: {formData.username}</h3>
+                <section className='form-body'>
+                    <label>username
+                        <input name='username' value={formData.username} onChange={handleChange}/>
+                    </label>
+                </section>
+                <footer>
+                    <button type='submit' disabled={disableButton}>Submit</button>
+                </footer>
             </form>
         </div>
     )
