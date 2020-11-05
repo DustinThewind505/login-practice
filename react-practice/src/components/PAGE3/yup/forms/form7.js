25b5754359268ff9a90ff8e997302e81c54fb115
unknown <Dustin.Guillen14@yahoo.com>
2020-11-05 14:45:49 -0700

    form7 functionality

--- react-practice/src/components/PAGE3/yup/forms/form7.js
+++ react-practice/src/components/PAGE3/yup/forms/form7.js
@@ -1,32 +1,64 @@
-import React from 'react';
-
-
+import React, { useState } from 'react';
+import * as yup from 'yup';
+// ========== COMPONENT ==========
+const formSchema = yup.object().shape({
+    password1: yup.string().length(8).required(),
+    password2: yup.string().length(8).required()
+})
 
 
 
 
 function Form7(props) {
     // ========== STATE ==========
+    const [disableButton, setDisableButton] = useState(true)
+
+    const [formData, setFormData] = useState({
+        password1: "",
+        password2: ""
+    })
 
 
 
     // ========== FUNCTION ==========
+    const handleChange = e => {
+        const newFormState = {
+            ...formData,
+            [e.target.name]: e.target.value
+        }
+
+        setFormData(newFormState)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        props.setDisplayForm(formData)
+
+        setFormData({
+            password1: "",
+            password2: ""
+        })
+    }
 
 
 
 
     // ========== COMPONENT ==========
-    return(
+    return (
         <div className='form-container'>
             <h3>Form #7</h3>
-            <p></p>
-            <form>
+            <p>input 1:{formData.password1}</p>
+            <p>input 2:{formData.password2}</p>
+            <form onSubmit={handleSubmit}>
                 <h3>Form #7</h3>
                 <section className='form-body'>
-                    <label>
-                        <input type='password' />
+                    <label>password
+                        <input type='password' name='password1' value={formData.password1} onChange={handleChange} />
                     </label>
-
+                    <label>confirm password
+                        <input type='password' name='password2' value={formData.password2} onChange={handleChange} />
+                    </label>
                 </section>
                 <footer>
                     <button type='submit'>Submit</button>
