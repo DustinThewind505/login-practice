08ebf9fe7dd58745e1abac398ddb8e04807d8107
unknown <Dustin.Guillen14@yahoo.com>
2020-11-05 15:25:31 -0700

    form validation for submit butt / input validation

--- react-practice/src/components/PAGE3/yup/forms/form7.js
+++ react-practice/src/components/PAGE3/yup/forms/form7.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
 // ========== COMPONENT ==========
 const formSchema = yup.object().shape({
-    password1: yup.string().length(8).required(),
-    password2: yup.string().length(8).required()
+    password1: yup.string().min(8).lowercase().required(),
+    password2: yup.string().min(8).lowercase().required()
 })
 
 
@@ -13,7 +13,14 @@ function Form7(props) {
     // ========== STATE ==========
     const [disableButton, setDisableButton] = useState(true)
 
+
+
     const [formData, setFormData] = useState({
+        password1: "",
+        password2: ""
+    })
+
+    const [errorsState, setErrorState] = useState({
         password1: "",
         password2: ""
     })
@@ -21,11 +28,28 @@ function Form7(props) {
 
 
     // ========== FUNCTION ==========
+    const validateChange = e => {
+        yup.reach(formSchema, e.target.name).validate(e.target.value)
+        .then(res => setErrorState({
+            ...errorsState,
+            [e.target.name]: ""
+        }))
+        .catch(err => setErrorState({
+            ...errorsState,
+            [e.target.name]: err.errors[0]
+        }))
+
+    }
+
     const handleChange = e => {
+        e.persist(e)
+
         const newFormState = {
             ...formData,
             [e.target.name]: e.target.value
         }
+
+        validateChange(e)
 
         setFormData(newFormState)
     }
@@ -42,26 +66,30 @@ function Form7(props) {
     }
 
 
-
+    useEffect(() => {
+        formSchema.isValid(formData).then(res => setDisableButton(!res))
+    }, [formData])
 
     // ========== COMPONENT ==========
     return (
         <div className='form-container'>
             <h3>Form #7</h3>
-            <p>input 1:{formData.password1}</p>
-            <p>input 2:{formData.password2}</p>
+            <p>input 1: {formData.password1}</p>
+            <p>input 2: {formData.password2}</p>
             <form onSubmit={handleSubmit}>
                 <h3>Form #7</h3>
                 <section className='form-body'>
                     <label>password
                         <input type='password' name='password1' value={formData.password1} onChange={handleChange} />
+    {errorsState.password1.length > 0 ? <p className='error'>{errorsState.password1}</p> : null}
                     </label>
                     <label>confirm password
                         <input type='password' name='password2' value={formData.password2} onChange={handleChange} />
+    {errorsState.password2.length > 0 ? <p className='error'>{errorsState.password2}</p> : null}
                     </label>
                 </section>
                 <footer>
-                    <button type='submit'>Submit</button>
+                    <button type='submit' disabled={disableButton}>Submit</button>
                 </footer>
             </form>
         </div>
