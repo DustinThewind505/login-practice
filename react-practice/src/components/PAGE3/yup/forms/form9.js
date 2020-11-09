9a492d00da965407689057e14690b6ba9258496e
unknown <Dustin.Guillen14@yahoo.com>
2020-11-08 17:50:13 -0700

    form9 validation

--- react-practice/src/components/PAGE3/yup/forms/form9.js
+++ react-practice/src/components/PAGE3/yup/forms/form9.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import * as yup from 'yup';
 // ========== Form validation with yup ==========
-const newSchema = yup.object().shape({
+const formSchema = yup.object().shape({
     username: yup.string().required('must enter username'),
     email: yup.string().email('must enter a valid email').required(),
-    jelly: yup.string(),
-    terms: yup.boolean()
+    jelly: yup.string().required('must choose a jelly'),
+    terms: yup.boolean().oneOf([true], 'must agree')
 })
 
 function Form9(props) {
@@ -17,15 +17,40 @@ function Form9(props) {
         terms: false
     })
 
+    const [errorState, setErrorState] = useState({
+        username: "",
+        email: "",
+        jelly: "",
+        terms: ""
+    })
+
 
     // ========== FUNCTION ==========
+    const validateChange = e => {
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+
+        yup.reach(formSchema, e.target.name).validate(value)
+        .then(res => setErrorState({
+            ...errorState,
+            [e.target.name]: ""
+        }))
+        .catch(err => setErrorState({
+            ...errorState,
+            [e.target.name]: err.errors[0]
+        }))
+    }
+
     const handleChange = e => {
+        e.persist()
+
         const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
 
         const newFormState = {
             ...formData,
             [e.target.name]: value
         }
+
+        validateChange(e)
 
         setFormData(newFormState)
     }
@@ -58,18 +83,22 @@ function Form9(props) {
                 <section className='form-body'>
                     <label>username
                         <input type='text' name='username' value={formData.username} onChange={handleChange} />
+                        {errorState.username.length > 0 ? <p className='error'>{errorState.username}</p> : null}
                     </label>
                     <label>email
                         <input type='email' name='email' value={formData.email} onChange={handleChange} />
+                        {errorState.email.length > 0 ? <p className='error'>{errorState.email}</p> : null}
                     </label>
-                    <label>Jelly
-                        <select name='jelly' onChange={handleChange}>
-                            <option>== Choose One ==</option>
-                            <option>Grape</option>
+                    <label>jelly
+                        <select name='jelly' value={formData.jelly} onChange={handleChange}>
+                            <option value="">== Choose One ==</option>
+                            <option value="grape">Grape</option>
                         </select>
+                        {errorState.jelly.length > 0 ? <p className='error'>{errorState.jelly}</p> : null}
                     </label>
-                    <label>Terms & Conditions
-                        <input type='checkbox' name='terms' checked={formData.terms} onChange={handleChange} />
+                    <label>terms & conditions
+                        <input type='checkbox' name='terms' onChange={handleChange} checked={formData.terms}/>
+                        {errorState.terms.length > 0 ? <p className='error'>{errorState.terms}</p> : null}
                     </label>
                 </section>
                 <footer>
