8a750e85f2891bbc4b891627d89a5428f85a7312
unknown <Dustin.Guillen14@yahoo.com>
2020-11-09 22:47:44 -0700

    form10 form validation

--- react-practice/src/components/PAGE3/yup/forms/form10.js
+++ react-practice/src/components/PAGE3/yup/forms/form10.js
@@ -1,34 +1,64 @@
-import React, { useState } from 'react';
-
-
+import React, { useEffect, useState } from 'react';
+import * as yup from 'yup';
+// ========== Form validation with yup ==========
+const formSchema = yup.object().shape({
+    select: yup.string().oneOf(['Human', 'Pickle'], 'must decide!')
+})
 
 
 function Form10(props) {
+    // ========== STATE ==========
+    const [disableButton, setDisableButton] = useState(true)
+
     const [formData, setFormData] = useState({
+        select: ""
+    })
+
+    const [errorState, setErrorState] = useState({
         select: ""
     })
 
 
 
 
+    // ========== FUNCTION ==========
+    const validateChange = e => {
+        yup.reach(formSchema, 'select').validate(e.target.value)
+            .then(res => setErrorState({ select: "" }))
+            .catch(err => setErrorState({ select: err.errors[0] }))
+    }
+
+
     const handleChange = e => {
+        e.persist()
+
         const newFormState = {
             select: e.target.value
         }
 
+        validateChange(e)
+
         setFormData(newFormState)
     }
-
 
 
     const handleSubmit = e => {
         e.preventDefault()
 
         props.setDisplayForm(formData)
+
+        setFormData({
+            select: ""
+        })
     }
 
+    useEffect(() => {
+        formSchema.isValid(formData).then(res => setDisableButton(!res))
+    }, [formData])
 
 
+
+    // ========== COMPONENT ==========
     return (
         <div className='form-container'>
             <h3>Form #10</h3>
@@ -43,10 +73,11 @@ function Form10(props) {
                             <option value='Human'>Human</option>
                             <option value='Pickle'>Pickle</option>
                         </select>
+                        {errorState.select.length > 0 ? <p className='error'>{errorState.select}</p> : null}
                     </label>
                 </section>
                 <footer>
-                    <button type='submit'>Submit</button>
+                    <button type='submit' disabled={disableButton}>Submit</button>
                 </footer>
             </form>
         </div>
