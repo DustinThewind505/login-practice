bcc74ed5c4cfc9a91df5860403aadcd5762ba03a
unknown <Dustin.Guillen14@yahoo.com>
2020-12-04 23:08:45 -0700

    made class component stateful

--- react-practice/src/components/PAGE1/classComponentForm/classComponentForm.js
+++ react-practice/src/components/PAGE1/classComponentForm/classComponentForm.js
@@ -3,23 +3,45 @@ import React from 'react';
 
 
 export default class ClassComponentForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            formData: {
+                username: "",
+                email: ""
+            }
+        }
+    }
 
+    handleChange = e => {
+        this.setState({
+            formData: { ...this.state.formData, [e.target.name]: e.target.value }
+          });
+    }
 
+    handleSubmit = e => {
+        e.preventDefault()
 
+        this.setState({formData:{username:"",email:""}})
+    }
 
 
     render() {
 
-        return(
+        return (
             <div className='form-container'>
                 <h3>Class Component Form</h3>
-                <form>
+                <p>Username: {this.state.formData.username}</p>
+                <p>Email: {this.state.formData.email}</p>
+                <form onSubmit={this.handleSubmit}>
                     <section className='form-body'>
                         <label>
-                            <input type='text' />
+                            <input
+                            type="text"
+                            name="username"value={this.state.formData.username} onChange={this.handleChange} />
                         </label>
                         <label>
-                            <input type='email' />
+                            <input type="email" name="email" value={this.state.formData.email} onChange={this.handleChange} required />
                         </label>
                     </section>
                     <footer>
