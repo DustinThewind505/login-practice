ec1fc2c223db14037f7906bd17bdceccadca9b8f
unknown <Dustin.Guillen14@yahoo.com>
2020-12-10 22:43:01 -0700

    yup input type time fnctionality

--- react-practice/src/components/PAGE3/yup/forms/form14.js
+++ react-practice/src/components/PAGE3/yup/forms/form14.js
@@ -1,29 +1,62 @@
 // ========== IMPORTS ==========
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
 // ========== From validation with yup ==========
+const fromSchema = yup.object().shape({
+    time: yup.string().required('Must enter a time')
+})
 
-function TimeInput() {
+function TimeInput(props) {
     // ========== STATE ==========
+    const [disableButton, setDisableButton] = useState(true)
+
+    const [formData, setFormData] = useState({
+        time: ""
+    })
+
+    const [errorState, setErrorState] = useState({
+        time: ""
+    })
 
 
     // ========== FUNCTIONS ==========
+    const handleChange = e => {
+        const newFormState = {
+            time: e.target.value
+        }
+
+        setFormData(newFormState)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        props.setDisplayForm(formData)
+
+        setFormData({
+            time: ""
+        })
+    }
+
+    useEffect(() => {
+        fromSchema.isValid(formData).then(res => setDisableButton(!res))
+    }, [formData])
 
 
     // ========== COMPONENT ==========
     return(
         <div className='form-container'>
             <h1>Form #14</h1>
-            <p>Time:</p>
-            <form>
+            <p>Time: {formData.time}</p>
+            <form onSubmit={handleSubmit}>
                 <h3>Input: time</h3>
                 <section className='form-body'>
                     <label>
-                        <input type='time' />
+                        <input type='time' name='time' value={formData.time} onChange={handleChange} />
                     </label>
                 </section>
                 <footer>
-                    <button type='submit'>Submit</button>
+                    <button type='submit' disabled={disableButton} >Submit</button>
                 </footer>
             </form>
         </div>
